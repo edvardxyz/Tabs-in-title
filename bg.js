b3c85e2b40c8093562c0dce5a75a68a803169161
Edvard <elsberg1995@protonmail.com>
2021-05-09 02:35:51 +0200

    changed to windows.update()

--- bg.js
+++ bg.js
@@ -5,6 +5,18 @@ browser.tabs.onActivated.addListener(listTabs);
 //browser.tabs.onDetached.addListener(listTabs);
 //browser.tabs.onAttached.addListener(listTabs);
 
+//var tempTitle;
+//var tabId = null;
+//
+//           if(tab.active){
+//               tempTitle = tab.title;
+//               tabId = tab.id;
+//          }
+//          if(tabId !== tab.id){
+//              changeTitelTemp(tabId, tempTitle);
+//          }
+
+
 function listTabs(){
     getCurrentWindowTabs().then((tabs) => {
         let tabsList = '';
@@ -12,15 +24,30 @@ function listTabs(){
         for (let tab of tabs){
             tabsList += tab.title;
         }
-        changeTitel(tabsList);
+        changeTitleWindow(tabs[0].windowId, tabsList);
     });
-}
-
-function changeTitel(title){
-    console.log(`document.title = '${title}';`);
-    browser.tabs.executeScript({code: `document.title = '${title}';`});
 }
 
 function getCurrentWindowTabs(){
     return browser.tabs.query({currentWindow: true});
 }
+
+function changeTitleWindow(id, title){
+    browser.windows.update(id, {titlePreface: title});
+}
+
+
+
+
+
+
+
+//function changeTitelTemp(id ,title){
+//    console.log(`document.title = '${title}';`);
+//    browser.tabs.executeScript(id, {code: `document.title = '${title}';`});
+//}
+//
+//function changeTitel(title){
+//    console.log(`document.title = '${title}';`);
+//    browser.tabs.executeScript({code: `document.title = '${title}';`});
+//}
